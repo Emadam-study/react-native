417d9a9d562a24046051863d1a23e48b4d8b0b47
emadam <ldy9037@naver.com>
2020-11-15 21:36:33 +0900

    - react native testing library 사용 튜토리얼 시작  - 기본 레이아웃 작성

--- App.js
+++ App.js
@@ -6,84 +6,25 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
 
-import SinglelineTextInputNativeComponent from 'react-native/Libraries/Components/TextInput/RCTMultilineTextInputNativeComponent';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
+import Profile from './src/Profile';
 
 const App: () => React$Node = () => {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (text) => {
-    setTodos([
-      ...todos,
-      {id: Math.random().toString(), textValue: text, checked: false}
-    ]);
-  };
-
-  const onRemove = (id) => (e) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-  const onToggle = (id) => (e) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? {...todo, checked: !todo.checked} : todo,
-      ),
-    );
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.appTitle}>Hello World</Text>
-      <View style={styles.card}>
-        <TodoInsert onAddTodo={addTodo} />
-        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Profile userName={'samslow'} name={'서현석'} />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#3143e8',
-  },
-  appTitle: {
-    color: '#fff',
-    fontSize: 36,
-    marginTop: 30,
-    marginBottom: 30,
-    fontWeight: '300',
-    textAlign: 'center',
-    backgroundColor: '#3143e8',
-  },
-  card: {
-    backgroundColor: '#fff',
-    flex: 1,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  input: {
-    padding: 20,
-    borderBottomColor: '#bbb',
-    borderBottomWidth: 1,
-    fontSize: 24,
-    marginLeft: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-
 
 export default App;
