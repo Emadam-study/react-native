034899a088e566ec3f2d834df27aa5089616caf6
emadam <ldy9037@naver.com>
2020-11-15 22:37:32 +0900

    - src/Profile.js 단위 테스트 작성 및 Coverage 확인

--- src/Profile.js
+++ src/Profile.js
@@ -3,7 +3,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import Greeting from './Greeting';
 
 const Profile = ({userName, name}) => {
-  const [msg, setMsg] = useState("Select your status");
+  const [msg, setMsg] = useState('Select your status');
 
   return (
     <View style={styles.container}>
